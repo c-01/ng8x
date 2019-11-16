428c92b12b716da577d3c8fd5779b296f64141df
Khung Long Bao Chua <emailcuamayban@gmail.com>
2019-11-16 17:31:10 +0700

    event binding

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-hello',
+  selector: "app-hello",
   template: `
-    <h3 [ngStyle]='{color: color, background:background}'>this is hello h3 {{ text }}</h3>
-    <h3 [ngStyle]='styleMore'>this is hello h3 {{ text }}</h3>
-    <p [class.width-border]='flagColor'>this tag p</p>
+    <h3 [ngStyle]="{ color: color, background: background }">
+      this is hello h3 {{ text }}
+    </h3>
+    <h3 [ngStyle]="styleMore">this is hello h3 {{ text }}</h3>
+    <p [class.width-border]="flagColor">this tag p</p>
 
-    <p [ngClass]= "{
-      'width-border': flagColor,
-      'other-class': true
-    }" >this tag as</p>
+    <p
+      [ngClass]="{
+        'width-border': flagColor,
+        'other-class': true
+      }"
+    >
+      this tag as
+    </p>
+
+    <button (click)="onChangeColor()">
+      {{ flagColor ? "Change" : "Changed" }} Color
+    </button>
   `
 })
 export class HelloComponent {
-  text = 'Name is Jon';
-  color = 'yellow';
-  background = 'red';
-  styleMore = {background: this.background, color: this.color};
+  text = "Name is Jon";
+  color = "yellow";
+  background = "red";
+  styleMore = { background: this.background, color: this.color };
   flagColor = true;
 
+  onChangeColor() {
+    // this.flagColor = false;
+    this.flagColor = !this.flagColor;
+    console.log(this);
+  }
 }
