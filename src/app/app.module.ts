97864efdd98eb84b58327bf9806814dc06b56b80
Khung Long Bao Chua <emailcuamayban@gmail.com>
2019-11-19 10:52:41 +0700

    install material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './components/hello/hello.component';
 import { LifeCycleComponent } from './components/life-cycle/life-cycle.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   // @Decorator
   declarations: [AppComponent, HelloComponent, LifeCycleComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
